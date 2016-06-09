e4bbbad4434e3f1773b3b6feca6424a1dae4bd70
Ricardo Sierra <sierra.csi@gmail.com>
2016-06-09 17:35:48 -0300

    Cut duplicate alias history

--- source/50_history.sh
+++ source/50_history.sh
@@ -6,13 +6,15 @@ shopt -s histreedit
 shopt -s histverify
 
 # Entries beginning with space aren't added into history, and duplicate
-# entries will be erased (leaving the most recent entry).
-export HISTCONTROL="ignorespace:erasedups"
+# Larger bash history (allow 32³ entries; default is 500)
+export HISTSIZE=50000000;
+export HISTFILESIZE=$HISTSIZE;
+export HISTCONTROL=ignoredups;
 # Give history timestamps.
 export HISTTIMEFORMAT="[%F %T] "
-# Lots o' history.
-export HISTSIZE=10000
-export HISTFILESIZE=10000
+# Make some commands not show up in history
+export HISTIGNORE=" *:ls:cd:cd -:pwd:exit:date:* --help:* -h:pony:pony add *:pony update *:pony save *:pony ls:pony ls *";
+
 
 # Easily re-execute the last history command.
 alias r="fc -s"
