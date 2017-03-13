c228dd7f48872a424a8083f7ca89bd32e15e62e0
Ricardo Sierra <sierra.csi@gmail.com>
2017-03-13 03:32:53 -0300

    Comentando bug

--- source/30_connection.sh
+++ source/30_connection.sh
@@ -1,35 +1,42 @@
-check-ssh-add() {
-if [ "$DESKTOP_SESSION" == "" ]; then
-  if [[ `ssh-add -l` != *id_?sa* ]]; then 
-    ssh-add -t 5h  ## 5 hour ssh-agent expiration
-  fi
-fi
-}
+#check-ssh-add() {
+#if [ "$DESKTOP_SESSION" == "" ]; then
+#  if [[ `ssh-add -l` != *id_?sa* ]]; then 
+#    ssh-add -t 5h  ## 5 hour ssh-agent expiration
+#  fi
+#fi
+#}
 
-slogin() {
-  check-ssh-add
-  /usr/bin/slogin $@
-}
-
-ssh() {
-  check-ssh-add
-  /usr/bin/ssh $@
-}
-
-scp() {
-  check-ssh-add
-  /usr/bin/scp $@
-}
-
-sftp() {
-  check-ssh-add
-  /usr/bin/sftp $@
-}
-
-git() {
-  check-ssh-add
-  /usr/bin/git $@
-}
+#slogin() {
+#  check-ssh-add
+#  /usr/bin/slogin $@
+#}
+#
+#ssh() {
+#  check-ssh-add
+#  /usr/bin/ssh $@
+#}
+#
+#scp() {
+#  check-ssh-add
+#  /usr/bin/scp $@
+#}
+#
+#sftp() {
+#  check-ssh-add
+#  /usr/bin/sftp $@
+#}
+#
+#git() {
+#  check-ssh-add
+#  QUOTE_ARGS=''
+#  for ARG in "$@"
+#  do
+#    QUOTE_ARGS="${QUOTE_ARGS} '${ARG}'"
+#  done
+#  echo "${*:2}"
+#  echo "${QUOTE_ARGS}"
+#  /usr/bin/git ${QUOTE_ARGS}
+#}
 
 youtube-dump() {
   if [ -e "$1" ] ; then
