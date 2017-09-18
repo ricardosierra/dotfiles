e6f202944ec95a42f91ebcab538498085f2c2c55
Ricardo R. Sierra <ricardo@ricardosierra.com.br>
2017-09-18 13:46:26 -0300

    Testando novos recursos no ruby

--- source/50_ruby.sh
+++ source/50_ruby.sh
@@ -1,10 +1,4 @@
 export PATH
 
-# rbenv init.
-PATH="$(path_remove $DOTFILES/vendor/rbenv/bin):$DOTFILES/vendor/rbenv/bin"
-
-if [[ "$(type -P rbenv)" && ! "$(type -t _rbenv)" ]]; then
-  eval "$(rbenv init -)"
-fi
 
 source /home/sierra/.rvm/scripts/rvm
